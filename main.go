a73b9e2c4f47fa744d0878c9046091259f4516db
Sharath V <sharathkrml@gmail.com>
2022-10-26 16:43:00 +0530

    array and slices

--- main.go
+++ main.go
@@ -3,21 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	age := 30
-	fmt.Print("hello, \n") //PrintLn adds \n by default
-	fmt.Print("world , ")
+	// array in go is fixed length
+	// var ages [3]int = [3]int{1, 2, 3}
+	var ages = [3]int{1, 2, 3}
+	names := [3]string{"hi", "hello", "gm"}
+	ages[0] = 100 //changing value by index
+	fmt.Println(ages, len(ages))
+	fmt.Println(names, len(names))
 
-	// println
-	fmt.Println("my age is", 30)
+	// slices - array without length
+	// they use arrays under the hood
 
-	// formatted strings
-	fmt.Printf("My Age is %v and my name is %v  \n", "30", "Sharath")
-	fmt.Printf("My Age is %q and my name is %q  \n", "30", "Sharath") // add "quotes"
-	fmt.Printf("type of age is %T \n", age)                           //Type of variable
-	fmt.Printf("float %0.2f  \n", 2.555)                              // float upto 2 decimal points
-	// Sprintf (Save formatted string!!)
-	formattedStr := fmt.Sprintf("My Age is %v and my name is %v  \n", "30", "Sharath")
+	var scores = []int{100, 200, 300, 400}
+	scores[2] = 20
+	scores = append(scores, 9999) // append creates a new slice,which is assigned to scores manually
+	fmt.Println(scores, len(scores))
 
-	fmt.Println(formattedStr)
+	// slice ranges
+	rangeOne := names[1:3] //index 1,2
+	rangeOne = names[0:2]  //index 0,1
+	fmt.Println(rangeOne)
+
+	rangeTwo := scores[1:3]
+	fmt.Println(rangeTwo)
+	letsseeslice := []string{"first", "second", "third", "fourth"}
+	rangeThree := letsseeslice[1:] //everything starting from 1st index
+
+	fmt.Println(rangeThree)
+	rangeAppended := append(rangeOne, "wohooo")
+	fmt.Println(rangeAppended)
 
 }
