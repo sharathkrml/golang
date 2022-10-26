1181c9160eab190bbfe4b4acdf2a57ab2168241f
Sharath V <sharathkrml@gmail.com>
2022-10-26 12:03:11 +0530

    variables with strings

--- main.go
+++ main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+var outsideFunction = "outsideFunction"
+
+// outsideFunctiontwo:= ":= be used outside function"
 func main() {
-	fmt.Println("hello ninjas")
+	//  string
+	var nameOne string = "hii"
+	var nameTwo = "sharath" // type inference
+	var nameThree string
+	nameFour := "SHARATH"
+
+	fmt.Println(nameOne+" "+nameTwo+" "+nameThree, nameFour)
+
+	nameThree = "hiiiiiiii"
+	fmt.Println(nameOne+" "+nameTwo+" "+nameThree, nameFour)
+
+	fmt.Println(outsideFunction)
 }
