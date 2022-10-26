0b00b4a06d0aa0d32286321f277373ea02cae84d
Sharath V <sharathkrml@gmail.com>
2022-10-26 21:09:56 +0530

    added struct bill

--- main.go
+++ main.go
@@ -3,21 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	age := 30
-	fmt.Print("hello, \n") //PrintLn adds \n by default
-	fmt.Print("world , ")
-
-	// println
-	fmt.Println("my age is", 30)
-
-	// formatted strings
-	fmt.Printf("My Age is %v and my name is %v  \n", "30", "Sharath")
-	fmt.Printf("My Age is %q and my name is %q  \n", "30", "Sharath") // add "quotes"
-	fmt.Printf("type of age is %T \n", age)                           //Type of variable
-	fmt.Printf("float %0.2f  \n", 2.555)                              // float upto 2 decimal points
-	// Sprintf (Save formatted string!!)
-	formattedStr := fmt.Sprintf("My Age is %v and my name is %v  \n", "30", "Sharath")
-
-	fmt.Println(formattedStr)
-
+	myBill := newBill("mario's bill")
+	fmt.Println(myBill)
 }
