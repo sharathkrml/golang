6d7696fffa8f81e6de092bfaae33a65fa4f570f7
Sharath V <sharathkrml@gmail.com>
2022-10-26 21:51:51 +0530

    interfaces

--- main.go
+++ main.go
@@ -1,12 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
+type square struct {
+	length float64
+}
+
+// interface
+type shape interface {
+	area() float64
+	circumf() float64
+}
+type circle struct {
+	radius float64
+}
+
+func (s square) area() float64 {
+	return s.length * s.length
+}
+func (s square) circumf() float64 {
+	return s.length * 4
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+func (c circle) circumf() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func printShapeInfo(s shape) {
+	fmt.Printf("area of %T is %f \n", s, s.area())
+	fmt.Printf("circumf of %T is %f \n", s, s.circumf())
+}
 func main() {
-	myBill := newBill("mario's bill")
-
-	myBill.updateTip(100)
-	myBill.addItem("biriyani", 150)
-	fmt.Println(myBill)
-	fmt.Println(myBill.format())
+	shapes := []shape{
+		square{length: 10},
+		circle{radius: 20},
+	}
+	for _, value := range shapes {
+		printShapeInfo(value)
+		fmt.Println("-----------")
+	}
 }
